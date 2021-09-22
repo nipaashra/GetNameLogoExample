094b290c3c87b48aa1839c16d67b4f81253b6bb9
Nipa Ashra <nipa.ashra@gmail.com>
2021-09-22 11:56:19 +0800

    first commit

---
+++ Example/Pods/GetLogoName.swift
@@ -0,0 +1,17 @@
+//
+//  GetLogoName.swift
+//  GetNameLogoExample
+//
+//  Created by Apple on 22/09/21.
+//
+
+import Foundation
+
+class GetLogoName{
+    public init(){
+        
+    }
+    public func getLogoName() -> String{
+        return "LogoName"
+    }
+}
---
+++ Example/Pods/Target Support Files/GetNameLogoExample/GetNameLogoExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GetNameLogoExample : NSObject
+@end
+@implementation PodsDummy_GetNameLogoExample
+@end
